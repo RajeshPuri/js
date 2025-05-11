286edd5020c1df41d57e72d5c58fa9bd9db98969
rajeshpuri <razeshpuri@gmail.com>
2025-05-11 10:03:25 +0000

    JS BASIC update

--- 02_basic/test.js
+++ 02_basic/test.js
@@ -1,21 +1,57 @@
-"use strict"; //treate all JS as newer version
+// "use strict"; //treate all JS as newer version
 
-// alert(2+3) // we are using nodejs , not browser
-
-
-let number =1
- number ='1dd'
-console.log(typeof number)
-
-console.log(Number(number))
+// // alert(2+3) // we are using nodejs , not browser
 
 
-console.log("****************")
-let isLoggined = undefined
+// let number =1
+//  number ='1dd'
+// console.log(typeof number)
+
+// console.log(Number(number))
 
 
-console.log(isLoggined)
-console.log(typeof isLoggined)
+// console.log("****************")
+// let isLoggined = undefined
 
-let BooleanIsLoggined =Boolean(isLoggined)
-console.log(BooleanIsLoggined)
+
+// console.log(isLoggined)
+// console.log(typeof isLoggined)
+
+// let BooleanIsLoggined =Boolean(isLoggined)
+// console.log(BooleanIsLoggined)
+
+
+// console.log("****************")
+// let obj = {
+//     name: "John",
+//     age: 30,
+//     isLoggined: false
+// }
+// console.log(obj)
+// console.log(typeof obj)
+// console.log(obj.name)
+// console.log(obj['name'])
+// console.log(obj.age)
+// console.log(obj['age'])
+// console.log(obj.isLoggined)
+// console.log(obj['isLoggined'])
+// console.log(obj['isLoggined'] = true)
+// console.log(obj.isLoggined)         
+
+
+// console.log("****************")
+
+
+// console.log(Number("123")); // 123
+// console.log(Number("abc")); // NaN
+// console.log(Number(undefined)); // NaN
+// console.log(Number(null)); // 0
+
+console.log(Boolean(0)); // false
+console.log(Boolean("")); // false
+console.log(Boolean(null)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean("hello")); // true
+console.log(Boolean(123)); // true
+console.log(Boolean({})); // true
+console.log(Boolean([])); // true
