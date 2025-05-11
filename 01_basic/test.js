783ce9c43670311bb20faef4967663dca18a0306
rajeshpuri <razeshpuri@gmail.com>
2025-05-11 09:49:25 +0000

    js

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,4 +1,14 @@
-console.log('Apple')
-console.log('Ball')
-console.log('Cat')
-console.log('Dog')
+const accountId = 1234
+let accountEmail = 'apple@ball.com'
+var accountPassword = '12356'
+name='rajes'
+let acountStatus
+let number =null
+
+// prefer not to use var
+// because of issue in block scope and functional scope 
+
+// accountId = 322
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,name,acountStatus,number])
---
+++ 02_basic/test.js
@@ -0,0 +1,21 @@
+"use strict"; //treate all JS as newer version
+
+// alert(2+3) // we are using nodejs , not browser
+
+
+let number =1
+ number ='1dd'
+console.log(typeof number)
+
+console.log(Number(number))
+
+
+console.log("****************")
+let isLoggined = undefined
+
+
+console.log(isLoggined)
+console.log(typeof isLoggined)
+
+let BooleanIsLoggined =Boolean(isLoggined)
+console.log(BooleanIsLoggined)
